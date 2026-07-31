1ce6d13a4c814f3df31db9b438efe6dfd2dec3ba
zhiyuzhang-0212 <zy.zhang0212@gmail.com>
2026-07-31 17:23:46 +0000

    Rename About section to About Me; drop doctoral-stage news item

--- assets/js/main.js
+++ assets/js/main.js
@@ -48,7 +48,7 @@
   var lang = document.documentElement.getAttribute("lang") || "en";
   if (lang !== "en" && lang !== "zh") lang = "en";
 
-  var VER = "10";
+  var VER = "11";
 
   /* ---------- Load + render ---------- */
   function boot() {
