fb668ae55a221145d04ab92fbd14a0e7cd20012e
zhiyuzhang-0212 <zy.zhang0212@gmail.com>
2026-08-01 03:06:20 +0000

    Restyle tags as tinted dot chips (distinct hue from link buttons)

--- assets/js/main.js
+++ assets/js/main.js
@@ -48,7 +48,7 @@
   var lang = document.documentElement.getAttribute("lang") || "en";
   if (lang !== "en" && lang !== "zh") lang = "en";
 
-  var VER = "14";
+  var VER = "15";
 
   /* ---------- Load + render ---------- */
   function boot() {
