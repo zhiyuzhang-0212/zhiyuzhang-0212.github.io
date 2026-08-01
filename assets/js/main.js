b6f3c1581fa185d5ec9aa3e45223dd98b4220604
zhiyuzhang-0212 <zy.zhang0212@gmail.com>
2026-08-01 11:58:43 +0000

    Contact: zh tagline 聊聊 -> 讨论

--- assets/js/main.js
+++ assets/js/main.js
@@ -48,7 +48,7 @@
   var lang = document.documentElement.getAttribute("lang") || "en";
   if (lang !== "en" && lang !== "zh") lang = "en";
 
-  var VER = "27";
+  var VER = "28";
 
   /* ---------- Load + render ---------- */
   function boot() {
